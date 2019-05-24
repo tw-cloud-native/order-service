c30dfa73750d1577978e288e5968f530a87e2a61
Liang <liachen@thoughtworks.com>
2019-05-24 12:04:45 +0800

    integrate spring cloud stream

--- src/main/java/com/dmall/orderservice/OrderServiceApplication.java
+++ src/main/java/com/dmall/orderservice/OrderServiceApplication.java
@@ -1,10 +1,12 @@
 package com.dmall.orderservice;
 
+import com.dmall.orderservice.adapter.mq.EventStreams;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -12,7 +14,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableCircuitBreaker
 @EnableFeignClients
 @EnableSwagger2
+@EnableBinding(EventStreams.class)
 public class OrderServiceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
---
+++ src/main/java/com/dmall/orderservice/domain/event/OrderCreatedEvent.java
@@ -0,0 +1,35 @@
+package com.dmall.orderservice.domain.event;
+
+import com.dmall.orderservice.domain.model.Order;
+import lombok.Builder;
+import lombok.Value;
+
+import java.math.BigDecimal;
+
+@Value
+@Builder
+public class OrderCreatedEvent {
+  private final String id;
+  private final long productId;
+  private final int quantity;
+  private final BigDecimal totalPrice;
+  private final String address;
+  private final String phoneNumber;
+  private boolean paid;
+  private String lockId;
+  private String eventType = "OrderCreated";
+
+  public static OrderCreatedEvent from(Order order) {
+
+    return OrderCreatedEvent.builder()
+      .id(order.getId())
+      .productId(order.getProductId())
+      .quantity(order.getQuantity())
+      .totalPrice(order.getTotalPrice())
+      .address(order.getAddress())
+      .phoneNumber(order.getPhoneNumber())
+      .paid(order.isPaid())
+      .lockId(order.getLockId())
+      .build();
+  }
+}
