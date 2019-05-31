c551b57c09787141b679f79dc4a297e5055efe6b
Liang <liachen@thoughtworks.com>
2019-05-31 13:07:26 +0800

    add todo for hystrix dashboard

--- src/main/java/com/dmall/orderservice/OrderServiceApplication.java
+++ src/main/java/com/dmall/orderservice/OrderServiceApplication.java
@@ -2,12 +2,15 @@ package com.dmall.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableFeignClients
+//TODO: enable hystrix and dashboard
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
