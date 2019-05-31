654ed6c39b460244fceebc0a5dfcec2203b2ab94
Liang <liachen@thoughtworks.com>
2019-05-31 13:13:02 +0800

    add hystrix dashboard

--- src/main/java/com/dmall/orderservice/OrderServiceApplication.java
+++ src/main/java/com/dmall/orderservice/OrderServiceApplication.java
@@ -2,15 +2,16 @@ package com.dmall.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableFeignClients
-//TODO: enable hystrix and dashboard
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
